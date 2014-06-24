c260749e36b2581cf869ec3425e960dae1b120f0
fredaander <freda.ander@gmail.com>
2014-06-24 16:06:50 -0500

    added Huffman Tree

--- Encode.java
+++ Encode.java
@@ -7,64 +7,155 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 
 
 public class Encode {
-	public static int[] probabilities = new int[26]; 
+
+	public static class Node {
+		Node left;			//children
+		Node right;
+		int probability;	//probability 
+		char letter; 		//letter we are encoding/decoding
+		String encode; 		//encode value
+		boolean isLeaf;
+		int childProb;		//child's smallest probability
+
+		public Node(int prob, char let, boolean leaf) {
+			probability = prob;
+			letter = let; 
+			isLeaf = leaf; 
+			encode = "";
+			childProb = 1000000;
+		}
+
+		@Override public String toString() {
+			return "(" + letter + ", " + probability +", " + encode + ")"; 
+		}
+		
+		public boolean hasRight(){
+			return (right == null) ? false: true; 
+		}
+		
+		public boolean hasLeft(){
+			return (left == null) ? false: true; 
+		}
+	}
+
+	public static int[] probabilities = new int[26];
+	public static Node huffmanTree; 
 	public static int sum = 0; 
-	
-	private static void setUpProbabilities(String file) {
+	public static String entropy = "";
+
+	/*
+	 * pre: reads in a file of probabilities (one number per line) and assigns to letter of alphabet
+	 * post: calls setUpTree to place probabilities in a Huffman Tree to encode letters
+	 */
+	private static void readInFile(String file, int start) {
+		
+		//create priorityqueue to store in nodes to be put into Huffman tree
+		PriorityQueue<Node> letters = new PriorityQueue<Node>(5, new Comparator<Node>() {
+			public int compare(Node n1, Node n2) {
+				int retvalue = Integer.valueOf(n1.probability).compareTo(n2.probability);
+				if (retvalue == 0)
+					return Integer.valueOf(n1.childProb).compareTo(n2.childProb);
+				return retvalue; 
+			}});
+
+		//read in the file
 		try {
-			BufferedReader reader = new BufferedReader(
-					new InputStreamReader(
-							new FileInputStream(file),
+			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream
+
+(file),
 							Charset.forName("UTF-8")));
 			String aLine;
-			int idx = 0; 
-		
+			int letterValue = start +97; 
+
+			//create a node for each line of data in file, place in min to max order
 			while ((aLine = reader.readLine()) != null){
 				int lineValue = Integer.parseInt(aLine);
-				probabilities[idx] = lineValue;
+				Node aNode = new Node(lineValue, (char) letterValue, true); 
+				letters.add(aNode); 
 				sum += lineValue;
-				idx++;
+				letterValue++;
 			}
-			entropy(); 
 			
 			reader.close();
+			setUpTree(letters);   //use priorityqueue to create Huffman tree
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	public static void entropy(){
-		int current = 97;
+
+	private static void setUpTree(PriorityQueue<Node> letters) {
 		
-		System.out.println("Result \t\tProbability \t\tEncoding");
-		String entropy = "H = -("; 
-		
-		for (int a : probabilities) {
-			if (a != 0) {
-				System.out.println("  " + (char) current + "\t\t  " + a + "/" + sum); 
-				entropy = entropy + a + "/" + sum + " log(" + a + "/" + sum + ") + ";
-			}
-			current++;
+		while (letters.size() > 1){
+			Node smallest = letters.remove();
+			appendEncodingDown(smallest,0); 
+			Node smaller  = letters.remove();
+			appendEncodingDown(smaller, 1); 
+			
+			Node parent = new Node(smallest.probability + smaller.probability, ' ', false);
+			parent.childProb = smallest.probability;
+			parent.left=smallest;
+			parent.right=smaller;
+			letters.add(parent); 
 		}
 		
-		System.out.println("\n" + entropy.substring(0, entropy.length()-3) + ")");
+		huffmanTree = letters.remove();
+	}
+
+	private static void appendEncodingDown(Node smaller, int i) {
+		if (smaller.isLeaf) { smaller.encode = i + smaller.encode; }
+		else {
+			smaller.encode = i + smaller.encode;
+			if (smaller.hasLeft()) appendEncodingDown(smaller.right, i);
+			if (smaller.hasRight()) appendEncodingDown(smaller.left, i); 
+		}
 	}
 	
-	public static void main(String[] args) {
-
-		//CountFrequencies.countLetters(args[0]);
-		setUpProbabilities(args[0]); 
-		createTestText(Integer.parseInt(args[1]));
-		
+	private static void printTree(Node aNode) {
+		if (aNode != null) {
+			if (aNode.hasLeft()) printTree(aNode.left);
+			System.out.println(aNode);
+			if (aNode.hasRight()) printTree(aNode.right); 
+		}
 	}
+	
+	private static void printEntropy(Node aNode) {
+		if (aNode.isLeaf){
+			System.out.println("  " + aNode.letter + "\t\t  " + aNode.probability + "/" + sum + "\t\t\t" + aNode.encode); 
+			entropy = entropy + aNode.probability + "/" + sum + " log(" + aNode.probability + "/" + 
+
+sum + ") + ";
+		}
+		else {
+			if (aNode.hasLeft()) printEntropy(aNode.left);
+			if (aNode.hasRight()) printEntropy(aNode.right); 
+		}
+	}
+
+	public static void entropy(){
+		System.out.println("Result \t\tProbability \t\tEncoding");
+		entropy = "H = -("; 
+		printEntropy(huffmanTree); 
+		entropy = entropy.substring(0, entropy.length()-3) + ")";
+		System.out.println("\n" + entropy);
+	}
+
+	
+	public static void main(String[] args) {
+		int start = 0; 
+		//CountFrequencies.countLetters(args[0]);
+		//setUpProbabilities(args[0]); 
+		readInFile(args[0], start); 
+		entropy(); 
+	}
+	
 	public static void createTestText(int k){
 		try {
 			File file = new File("testText.txt");
