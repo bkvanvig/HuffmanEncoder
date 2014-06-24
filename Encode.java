49a509d09b5eec03506c6eae379d28b40839be77
Brittany Kvanvig <brittany.kvanvig@utexas.edu>
2014-06-24 13:14:04 -0500

    added testText generator

--- Encode.java
+++ Encode.java
@@ -1,6 +1,9 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
@@ -57,7 +60,94 @@ public class Encode {
 	
 	public static void main(String[] args) {
 
-		CountFrequencies.countLetters(args[0]);
-		//setUpProbabilities(args[0]); 
+		//CountFrequencies.countLetters(args[0]);
+		setUpProbabilities(args[0]); 
+		createTestText(Integer.parseInt(args[1]));
+		
+	}
+	public static void createTestText(int k){
+		try {
+			File file = new File("testText.txt");
+			if (!file.exists())
+			{
+				file.createNewFile();
+			}
+			
+			BufferedWriter bw = new BufferedWriter(new FileWriter(file.getAbsoluteFile()));
+			while (k>0)
+			{
+				bw.write(findLetter());
+				k--;
+			}
+			bw.close();
+			System.out.println("Done");
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+	public static String findLetter(){
+		int min = 1;
+		int max = sum;
+		int target = min + (int)(Math.random() * ((max - min) + 1));
+		int runningsum = 0;
+		//System.out.println(target);
+		
+		if (target >= min && target<(runningsum += probabilities[0]))
+			return "a";
+		if (target >= min && target<(runningsum += probabilities[1]))
+			return "b";
+		if (target >= min && target<(runningsum += probabilities[2]))
+			return "c";
+		if (target >= min && target<(runningsum += probabilities[3]))
+			return "d";
+		if (target >= min && target<(runningsum += probabilities[4]))
+			return "e";
+		if (target >= min && target<(runningsum += probabilities[5]))
+			return "f";
+		if (target >= min && target<(runningsum += probabilities[6]))
+			return "g";
+		if (target >= min && target<(runningsum += probabilities[7]))
+			return "h";
+		if (target >= min && target<(runningsum += probabilities[8]))
+			return "i";
+		if (target >= min && target<(runningsum += probabilities[9]))
+			return "j";
+		if (target >= min && target<(runningsum += probabilities[10]))
+			return "k";
+		if (target >= min && target<(runningsum += probabilities[11]))
+			return "l";
+		if (target >= min && target<(runningsum += probabilities[12]))
+			return "m";
+		if (target >= min && target<(runningsum += probabilities[13]))
+			return "n";
+		if (target >= min && target<(runningsum += probabilities[14]))
+			return "o";
+		if (target >= min && target<(runningsum += probabilities[15]))
+			return "p";
+		if (target >= min && target<(runningsum += probabilities[16]))
+			return "q";
+		if (target >= min && target<(runningsum += probabilities[17]))
+			return "r";
+		if (target >= min && target<(runningsum += probabilities[18]))
+			return "s";
+		if (target >= min && target<(runningsum += probabilities[19]))
+			return "t";
+		if (target >= min && target<(runningsum += probabilities[20]))
+			return "u";
+		if (target >= min && target<(runningsum += probabilities[21]))
+			return "v";
+		if (target >= min && target<(runningsum += probabilities[22]))
+			return "w";
+		if (target >= min && target<(runningsum += probabilities[23]))
+			return "x";
+		if (target >= min && target<(runningsum += probabilities[24]))
+			return "y";
+		if (target >= min && target<=(runningsum += probabilities[25]))
+			return "z";
+		else
+			return "ERROR";
 	}
 }
